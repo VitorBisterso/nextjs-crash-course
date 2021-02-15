b3a094bcb0714222cac34889936279cb79d4c496
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-02-15 15:45:37 -0300

    Add code from video

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
+      </h1>
+      <p className={headerStyles.description}>Keep up to date with the latest web dev news</p>
+    </div>
+  )
+}
+
+/* conditional styling
+<style jsx>
+  {`
+    .title {
+      color: ${condition};
+    }
+  `}
+</style>
+*/
+
+export default Header
